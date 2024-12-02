e414af4f69e97a917fddffce167eaad50a54578a
Robert Weber <robert.weber.1@gmx.net>
2024-12-02 22:11:14 +0100

    day 2 part 2

--- 2024/src/day2.cc
+++ 2024/src/day2.cc
@@ -9,75 +9,100 @@
 #include <map>
 #include <algorithm>
 #include <cmath>
+#include <vector>
+#include <fmt/format.h>
+#include <fmt/ranges.h>
 
 const MemoryMappedFile example(FileDescriptor("./input/day2/example.txt"));
 const MemoryMappedFile data(FileDescriptor("./input/day2/input.txt"));
 
+template<class Range>
+bool is_save_report(Range && report)
+{
+	bool safe = true;
+	bool first = true;
+	int prev_div = 0;
+	std::ranges::find_if(
+		report
+		| std::ranges::views::slide(2),
+		[&safe, &first, &prev_div](auto && sub)
+		{
+			int a = *sub.begin();
+ 			int b = *(++sub.begin());
 
-int count_safe_reports(std::string_view input, int damp_count = 0)
+			int diff = a - b;
+
+			if(std::signbit(prev_div) != std::signbit(diff) and not first)
+			{
+				safe = false;
+				return true;
+			}
+
+			if(std::abs(diff) == 0 or std::abs(diff) > 3)
+			{
+				safe = false;
+				return true;
+			}
+
+			prev_div = diff;
+			first = false;
+
+			return false;
+		});
+
+	return safe;
+}
+
+
+std::vector<int> remove_element(std::vector<int> in, int i)
+{
+	in.erase(in.begin() + i, in.begin() + i + 1);
+	return in;
+}
+
+
+int count_safe_reports(std::string_view input, Part part)
 {
 	int count = 0;
-	for_each_line(input,
-		[&count, &damp_count](std::string_view line)
+	for_each_line(
+		input,
+		[&count, part](std::string_view line)
 		{
-			bool ok = true;
+			std::vector<int> report;
 			std::ranges::for_each(
 				line
 				| make_splitter(" "sv)
-				| std::ranges::views::transform(string_view_to_int)
-				| std::ranges::views::slide(2)
-				| std::ranges::views::transform(
-					[](auto && subrange)
-					{
-						int a = *subrange.begin();
-						int b = *(++subrange.begin());
-						std::cout << "initial a: " << a << ", b: " << b << std::endl;
-
-						return b - a;
-					})
-				| std::ranges::views::slide(2)
-				,
-				[&ok, &damp_count](auto && subrange)
+				| std::ranges::views::transform(string_view_to_int),
+				[&report](int a)
 				{
-					int a = *subrange.begin();
-					int b = *(++subrange.begin());
-					std::cout << "second x: " << a << ", y: " << b << std::endl;
-
-					bool bad = false;
-					if(std::abs(a) == 0 or std::abs(a) > 3)
-						bad = true;
-
-					if(std::abs(b) == 0 or std::abs(b) > 3)
-						bad = true;
-
-					if(std::signbit(a) != std::signbit(b))
-						bad = true;
-
-					if(bad and damp_count == 0)
-					{
-						ok = false;
-					}
-
-					if(bad and damp_count != 0)
-					{
-						std::cout << "discard first bad value" << std::endl;
-						damp_count--;
-					}
+					report.push_back(a);
 				});
-			std::cout << "line:" <<  line <<  (ok?" Safe":" Unsafe") << std::endl;
-			if(ok)
-				count++;
+
+			auto safe = is_save_report(report);
+			count += safe;
+
+			if(not safe and part == Part::two)
+			{
+				for(int skip = 0; skip < report.size(); ++skip)
+				{
+					safe = is_save_report(remove_element(report, skip));
+
+					if(safe)
+						break;
+				}
+				count += safe;
+			}
 		});
 
 	return count;
 }
 
 
-
 TEST_CASE("day 2")
 {
-	REQUIRE(count_safe_reports(example.get()) == 2);
-	// REQUIRE(count_safe_reports(data.get()) == 463);
-	puts("......");
-	REQUIRE(count_safe_reports(example.get(), 2) == 4);
+	REQUIRE(count_safe_reports(example.get(), Part::one) == 2);
+	REQUIRE(count_safe_reports(data.get(), Part::one) == 463);
+
+	REQUIRE(count_safe_reports(example.get(), Part::two) == 4);
+	REQUIRE(count_safe_reports(data.get(), Part::two) == 514);
 }
