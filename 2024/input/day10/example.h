fe3a045b43d433aedeb90f5a2dd94b63e4200881
Robert Weber <robert.weber.1@gmx.net>
2024-12-10 06:37:28 +0100

    day 10

---
+++ 2024/input/day10/example.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <string_view>
+
+using namespace std::literals;
+
+namespace day10 {
+
+struct Example
+{
+	static constexpr std::string_view data[] = {
+		"89010123",
+		"78121874",
+		"87430965",
+		"96549874",
+		"45678903",
+		"32019012",
+		"01329801",
+		"10456732"
+	};
+};
+
+} //namespace day10
---
+++ 2024/src/day10.cc
@@ -0,0 +1,28 @@
+#include <catch2/catch_test_macros.hpp>
+#include <read_file.h>
+#include <utils.h>
+#include <fmt/format.h>
+#include <fmt/ranges.h>
+#include <ctre.hpp>
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <functional>
+#include <map>
+#include <fmt/ostream.h>
+#include <set>
+
+#include <day10/example.h>
+#include <day10/input.h>
+
+
+TEST_CASE("day 10")
+{
+	SECTION("part 1")
+	{
+		REQUIRE(false);
+	}
+
+	SECTION("part 2")
+	{}
+}
