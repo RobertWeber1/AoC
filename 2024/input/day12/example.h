9b8e76981b3e8b33daabde89509b82c35aac8d8a
Robert Weber <robert.weber.1@gmx.net>
2024-12-12 06:43:23 +0100

    WIP: day 12

---
+++ 2024/input/day12/example.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <string_view>
+
+using namespace std::literals;
+
+namespace day12 {
+
+struct Example
+{
+	static constexpr std::string_view data[] = {
+		"RRRRIICCFF",
+		"RRRRIICCCF",
+		"VVRRRCCFFF",
+		"VVRCCCJFFF",
+		"VVVVCJJCFE",
+		"VVIVCCJJEE",
+		"VVIIICJJEE",
+		"MIIIIIJJEE",
+		"MIIISIJEEE",
+		"MMMISSJEEE",
+	};
+};
+
+} //namespace day12
---
+++ 2024/src/day12.cc
@@ -0,0 +1,31 @@
+#include <catch2/catch_test_macros.hpp>
+#include <read_file.h>
+#include <utils.h>
+#include <fmt/format.h>
+#include <fmt/ranges.h>
+#include <ctre.hpp>
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <functional>
+#include <map>
+#include <fmt/ostream.h>
+#include <set>
+#include <point.h>
+#include <variant>
+
+#include <day12/example.h>
+#include <day12/input.h>
+
+TEST_CASE("day 12")
+{
+	SECTION("part 1")
+	{
+		REQUIRE(false);
+	}
+
+	SECTION("part 2")
+	{
+	}
+}
+
